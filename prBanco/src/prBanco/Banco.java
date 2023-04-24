1ac090154aab1871dbd5c5e148abfc305e737690
unknown <frp0007@alu.medac.es>
2023-04-24 11:27:22 +0200

    Añade get and set

--- prBanco/src/prBanco/Banco.java
+++ prBanco/src/prBanco/Banco.java
@@ -7,4 +7,19 @@ double saldo;
 		this.iban = iban;
 		this.saldo = saldo;
 	}
+	public int getIban (){
+	     return iban;
+	}
+
+	public void setAtributo (int iban){
+	    this.iban = iban;
+	}
+	public double getSaldo (){
+	     return saldo;
+	}
+
+	public void setSaldo (double saldo){
+	    this.saldo = saldo;
+	}
+	
 }
